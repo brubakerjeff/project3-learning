bcc58f568617438706c5087a3065baba6a8a4cc6
Jeff Brubaker <brubaker.jeff@gmail.com>
2025-05-31 13:50:30 -0400

    updates

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,10 @@
+# CMake generated Testfile for 
+# Source directory: /home/jeffoldubuntu/catkin_ws/src
+# Build directory: /home/jeffoldubuntu/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("my_robot")
+subdirs("ball_chaser")
+subdirs("gazebo_ros_2Dmap_plugin")
